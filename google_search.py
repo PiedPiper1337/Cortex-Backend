5fff8f4248eb08e34dfa58990ab02758c8a70e2a
jon-bassi <jonbassi10@gmail.com>
2016-03-07 15:56:30 -0800

    final update

--- google_search.py
+++ google_search.py
@@ -1,19 +1,22 @@
 from googlesearch import GoogleSearch
 from readability.readability import Document
 from bs4 import BeautifulSoup
+import re
 import sys
 import requests
 import urllib
+
+def remove_non_ascii(text):
+    return ''.join(i for i in text if ord(i)<128)
+
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print 'no urls found'
 		sys.exit(0)
-
 	query = ''
         for i in range (1,len(sys.argv)):
                 query += sys.argv[i] + ' '
-
 	gs = GoogleSearch(query)
 	if len(gs.top_urls()) < 1:
 		print 'no urls found'
@@ -23,4 +26,4 @@ if __name__ == "__main__":
 #		print url
 	html = urllib.urlopen(urls[0]).read()
 	soup = BeautifulSoup(Document(html).summary(), "lxml")
-	print soup.get_text()[0:1000]
+	print remove_non_ascii(soup.get_text()[0:1000])
